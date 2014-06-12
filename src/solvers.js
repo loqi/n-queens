49b9d4c7aa77f35db46ca3957e00b8e962083fd5
Hack Reactor Students <accounts@hackreactor.com>
2014-06-11 19:45:32 -0700

    Rooks works now. Time to do Queens.

--- src/solvers.js
+++ src/solvers.js
@@ -14,22 +14,32 @@
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  var solution = [];
+  var row;
+  for (var i = 0; i < n; i++) {
+    row = [];
+    for (var j = 0 ; j < n; j++) { row[j] = 0; }
+    row[i] = 1;
+    solution.push(row);
+  }
   return solution;
 };
 
-
-
-// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
-
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+// Fast factorial function for positive integers.
+var fact = function(n) {
+  if (isNaN(n) || n < 0) return undefined;
+  var f = 1;
+  while (n > 1) { f *= n--; }
+  return f;
 };
 
+// return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
+window.countNRooksSolutions = fact;
+// window.countNRooksSolutions = function(n) {
+//   var solutionCount = fact(n);
+//   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+//   return solutionCount;
+// };
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
